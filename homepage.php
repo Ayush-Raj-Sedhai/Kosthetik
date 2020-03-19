60464ce4f1cab2a64b32b1b899bc53941da392c5
Ayush Raj Sedhai <ayushsedhai@gmail.com>
2020-03-19 16:57:52 +0545

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,142 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.toptopnav {
+  overflow: hidden;
+  background-color: #E0E5E5  ;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #E0E5E5 ;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+ 
+}
+
+</style>
+</head>
+<body>
+<div>
+<div  class="toptopnav">
+<!-- <div style="width:150px; height:90px;float:right;margin-right:20px"> -->
+<input type="image" src="logo.jpg" style="width:120px;float:left height:50px">
+
+<p style="float:right;margin-top:25px; margin-right:20px"><a href="#Signup">IND</a></p>
+<p style="float:right;margin:25px"><a href="#login">BGD</a></p>
+<p style="float:right;margin-top:25px"><a href="#login">NEP</a></p>
+
+</div>
+
+
+<div class="topnav">
+  <a href="#home">About Us  ।</a>
+  <a href="#about">Beauty Clinics & Spas  । </a>
+  <a href="#contact">Partnerships  ।</a>
+  <a href="#contact">Blogs  ।</a>
+  <a href="#contact">Contacts  ।</a>
+ <div class="search-container">
+    <!-- <form action="/action_page.php"> -->
+      <!-- <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button> -->
+      <p style="float:right"><a href="#Signup">Sign up</a></p>
+      <p style="float:right"><a href="#login">Login</a></p>
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+      </div>
+      
+     </div>
+    <!-- </form> -->
+  <!-- </div> -->
+  <div class="container-fluid">
+  <div class="row">
+<div class="col-md-6" style="background-color: green; height:200px; width:10px">
+Yetaa lekhaa aani
+</div>
+<div class="col-md-6">
+Yetaa lekhaa aani
+</div>
+</div>
+</div>
+<div class="container-fluid">
+  <div class="row">
+<div class="col-md-6">
+Yetaa lekhaa aani
+</div>
+<div class="col-md-6">
+Yetaa lekhaa aani
+</div>
+</div>
+</div>
+
+</body>
+</html>
